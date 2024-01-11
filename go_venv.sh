18e7aafcc734609631cd5499f656a79d1b7153c0
Pascal Guttmann <git@pascal-guttmann.de>
2024-01-11 11:41:00 +0100

    add venv activation script for *nix like systems

---
+++ go_venv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+# ******************************************************************************
+#
+# author: P. Guttmann
+#
+# This script can be `sourced` to activate the python venv, and if necessary
+# create the local python venv and install the dependencies required by
+# `requirements.txt`.  The storage location of the created python venv, and the
+# implicitly assumed directory to activate the python venv is `.venv`.
+#
+# Usage:
+#     . ./go_venv.sh
+#
+# ******************************************************************************
+
+venv_dir=./.venv
+venv_prompt=venv
+venv_activate=$venv_dir/Scripts/activate
+venv_requirements=requirements.txt
+
+if ! [ -f "$venv_activate" ]; then
+    # python venv does not exist, so create it
+
+    # create the python venv locally
+    python -m venv --prompt $venv_prompt $venv_dir
+
+    # activate the venv
+    source $venv_activate
+
+    # install dependencies
+    pip install -r $venv_requirements
+else
+    # python venv does exist
+    source $venv_activate
+fi
