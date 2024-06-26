77e4a6f93c94143d9635226bcceab265c9e9cada
pascalguttmann <github@pascal-guttmann.de>
2024-06-27 01:15:36 +0200

    add numerator order setting for tfest

--- docs/workspace/pss/control/tfest.py
+++ docs/workspace/pss/control/tfest.py
@@ -1,7 +1,7 @@
 import control as ct
 from scipy.optimize import least_squares
 
-def tfest(order, frd, num0=None, den0=None):
+def tfest(order, frd, num0=None, den0=None, orderNum=None):
     """Estimates a complex valued transfer function of order 'order' for frequency
     response data 'frd' via least squares
 
@@ -22,7 +22,7 @@ def tfest(order, frd, num0=None, den0=None):
 
     def unpackArg(p):
         halfIndex = len(p)//2
-        return p[:halfIndex], p[halfIndex:]
+        return p[:orderNum+1], p[orderNum+1:]
 
     def residuals(p, frd, order):
         """Wrapper function to adhere to function definition of
@@ -37,17 +37,21 @@ def tfest(order, frd, num0=None, den0=None):
         residuals = [abs(a - b) for a, b in zip(H_calc, frd["H"])]
         return residuals
 
+    if orderNum is None:
+        orderNum = order
+
     if num0 is None:
-        num0 = [0.0] * (order + 1)
+        num0 = [0.0] * (orderNum + 1)
         num0[-1] = 1.0
 
     if den0 is None:
         den0 = [0.0] * (order + 1)
         den0[-1] = 1.0
 
+
     p0 = packArg(num0, den0)
 
-    p = least_squares(residuals, p0, method='trf', loss='soft_l1', max_nfev=1e5,
+    p = least_squares(residuals, p0, method='trf', loss='soft_l1', max_nfev=1e4,
                       xtol=1e-15, ftol=1e-15,
                       verbose=2, args=(frd, order))
 
@@ -69,7 +73,7 @@ if __name__=="__main__":
     H = mag * e**(1j * phase)
     frd = {"omega": list(omega), "H": list(H)}
 
-    num, den = tfest(2, frd, den0=[0.8, 0, 0.8])
+    num, den = tfest(2, frd, orderNum=1)
     sys_model = ct.tf(num, den)
 
     print(f"{num=} {den=}")
