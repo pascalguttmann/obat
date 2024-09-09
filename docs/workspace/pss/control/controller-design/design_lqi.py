6cfa3c05a21d64ca79455e176fb87d7597d8f2d5
pascalguttmann <github@pascal-guttmann.de>
2024-09-09 15:43:51 +0200

    add lqi design

--- docs/workspace/pss/control/controller-design/design_lqi.py
+++ docs/workspace/pss/control/controller-design/design_lqi.py
@@ -1,4 +1,99 @@
-from math import pi
-
 import control as ct
 import matplotlib.pyplot as plt
+import numpy as np
+
+from lqi import lqi, ss2ABCD
+
+sysOmega = 10e6
+sysPowerElectronics: ct.StateSpace = ct.StateSpace(
+    ct.ss(
+        ct.zpk([], [-sysOmega], sysOmega),
+        inputs="u",
+        outputs="y",
+        name="plant",
+    )
+)
+
+stateWeight = np.eye(2) * 1
+inputWeight = np.eye(1) * 1
+crossWeight = None
+
+
+K, S, E = lqi(
+    *ss2ABCD(sysPowerElectronics),
+    Q=stateWeight,
+    R=inputWeight,
+    N=crossWeight,
+)
+
+print(K)
+print(E)
+
+sysController: ct.StateSpace = ct.StateSpace(
+    ct.ss(
+        np.zeros((0, 0)),
+        np.zeros((0, 2)),
+        np.zeros((1, 0)),
+        -K,
+        inputs=["plantStates", "performanceMeasure"],
+        outputs="u",
+        name="controller",
+    )
+)
+# print(sysController)
+
+sysIntegrator: ct.StateSpace = ct.StateSpace(
+    ct.ss(
+        [0],
+        [1],
+        [1],
+        [0],
+        inputs="e",
+        outputs="performanceMeasure",
+        name="integrator",
+    )
+)
+# print(sysIntegrator)
+
+sysSummingJunction: ct.StateSpace = ct.summing_junction(
+    inputs=["r", "-y"],
+    output="e",
+    name="summingJunction",
+)
+# print(sysSummingJunction)
+
+sysClosedLoop: ct.StateSpace = ct.interconnect(
+    syslist=[
+        sysSummingJunction,
+        sysIntegrator,
+        sysController,
+        sysPowerElectronics,
+    ],
+    connections=[
+        ["plant.u", "controller.u"],
+        ["controller.plantStates", "plant.y"],
+        ["controller.performanceMeasure", "integrator.performanceMeasure"],
+        ["integrator.e", "summingJunction.e"],
+        ["summingJunction.y", "plant.y"],
+    ],
+    inplist=["summingJunction.r"],
+    outlist=[["plant.y"], ["controller.u"]],
+    inputs=["r"],
+    outputs=["y", "u"],
+    name="closeLoop",
+    check_unused=True,
+    warn_duplicate=True,
+    debug=False,
+)
+# print(sysClosedLoop)
+
+ct.step_response(sysClosedLoop).plot(
+    overlay_signals=True,
+)
+plt.show()
+# ct.pzmap(sysClosedLoop, plot=True)
+# plt.show()
+# ct.nyquist_plot(sysClosedLoop, plot=True)
+# plt.show()
+# ct.bode_plot(sysClosedLoop, plot=True)
+# plt.show()
