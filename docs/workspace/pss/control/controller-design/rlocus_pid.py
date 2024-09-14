5ab59270263bed120231b56c5d9dc2d1242716e7
pascalguttmann <github@pascal-guttmann.de>
2024-09-14 20:43:12 +0200

    normalize plot to omega plant = 1

--- docs/workspace/pss/control/controller-design/rlocus_pid.py
+++ docs/workspace/pss/control/controller-design/rlocus_pid.py
@@ -2,7 +2,7 @@ import control as ct
 import matplotlib.pyplot as plt
 import numpy as np
 
-plantOmega = 10e6
+plantOmega = 1
 plant = ct.zpk([], [-plantOmega], plantOmega)
 
 zero = [-plantOmega * 0.9, -plantOmega * 0.4]
